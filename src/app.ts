4cb1286ad1f7921c48e698357a964d3a2012cf0a
Daniel Mojica <danielrmojicac@gmail.com>
2023-02-12 18:52:54 -0300

    added tests and fixed messages structure

--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,38 @@
-import express from "express";
-import Websocket from "ws";
-import { BookInterface, Order } from "./types/sockets";
-import findIndexByProperty from "./utils/findIndex";
-import { getPrice } from "./controllers/getPrice";
-import { effectivePrice } from "./controllers/effectivePrice";
-import { checkPair } from "./middlewares/checkPair";
+import express, { query } from 'express';
+import Websocket from 'ws';
+import { getPrice } from './controllers/getPrice';
+import { effectivePrice } from './controllers/effectivePrice';
+import { checkPair } from './middlewares/checkPair';
+import { handleMessage } from './utils/handleMessage';
+
 const app = express();
-const ws = new Websocket("wss://api-pub.bitfinex.com/ws/2");
-const pairs = {
-  ETHUSD: "ETHUSD",
-  BTCUSD: "BTCUSD",
-};
+const ws = new Websocket('wss://api-pub.bitfinex.com/ws/2');
 
 const msg1 = JSON.stringify({
-  event: "subscribe",
-  channel: "book",
-  symbol: "tBTCUSD",
+  event: 'subscribe',
+  channel: 'book',
+  symbol: 'tBTCUSD',
 });
 
 const msg2 = JSON.stringify({
-  event: "subscribe",
-  channel: "book",
-  symbol: "tETHUSD",
+  event: 'subscribe',
+  channel: 'book',
+  symbol: 'tETHUSD',
 });
-
-export const book: BookInterface = {
-  BTCUSD: {
-    bids: [],
-    asks: [],
-  },
-  ETHUSD: {
-    bids: [],
-    asks: [],
-  },
-};
-const chanId = { BTCUSD: 0, ETHUSD: 0 };
 
 ws.onopen = () => {
   ws.send(msg1);
   ws.send(msg2);
 };
 
-// ws.onmessage = (msg) => checkMessage(msg, 'BTCUSD');
-ws.onmessage = (msg) => {
-  const data = JSON.parse(msg.data as string);
-  // console.log(data);
+ws.onmessage = (msg) => handleMessage(msg);
 
-  if (data.event) {
-    if (data.event === "subscribed") {
-      chanId[data.pair] = data.chanId;
-    }
-    //guardar el chanId, si data.pair === 'ETHUSD', chanIdETH = data.pair y lo mismo con BTC
-    return;
-  }
-  const [channelId, orders] = data;
-  const pair = chanId.BTCUSD === channelId ? pairs.BTCUSD : pairs.ETHUSD;
-  if (orders.length > 3) {
-    //this is the initial book
-    book[pair].asks = orders.filter((order: Order) => order[2] < 0);
-    book[pair].bids = orders.filter((order: Order) => order[2] > 0);
-    return;
-  } else {
-    //this is an update
-    // console.log(`actualizacion de ${pair}`, orders);
-    const orderType = orders[2] > 0 ? "bids" : "asks";
-    if (orders === "hb") return;
-    const foundIdx = findIndexByProperty(orders, pair, orderType, book);
-    if (foundIdx > -1) {
-      //found the price
-      if (orders[1] === 0) {
-        //i have to delete the price
-        // console.log('hay que eliminar el price', orders[0]);
-        book[pair][orderType] = book[pair][orderType].filter((item: Order) => item[0] !== orders[0]);
-        // console.log('eliminao');
-      } else {
-        // not 0, add to the book
-        book[pair][orderType][foundIdx] = orders;
-      }
-      // console.log(
-      //   `BOOK ${pair} ACTUALIZAO con nuevo ${orderType}`,
-      //   book[pair][orderType]
-      // );
-    } else {
-      book[pair][orderType].push(orders);
-    }
-    return;
-  }
-};
+app.use('/api', checkPair);
 
-app.get("/", (_req, res) => {
-  res.send("Hello!");
+app.get('/', (_req, res) => {
+  res.sendStatus(200);
 });
+app.get('/api/price', getPrice);
+app.get('/api/calcprice', effectivePrice);
 
-app.use(checkPair);
-
-app.get("/price", getPrice);
-app.get("/calcprice", effectivePrice);
-app.get("/book", (req, res) => {
-  const { pair } = req.query;
-
-  return res.send(book[pair as string]);
-});
-
-app.listen(3000, () => console.log("holis el server ya ta andando"));
 export default app;
--- src/controllers/effectivePrice.ts
+++ src/controllers/effectivePrice.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-const */
-import { book } from '../app';
 import { RequestHandler } from 'express';
 import { BookInterface, Order } from '../types/sockets';
+import { book } from '../utils/handleMessage';
 
 export const effectivePrice: RequestHandler = (req, res) => {
   // check ETHUSD o BTCUSD
@@ -15,9 +15,9 @@ export const effectivePrice: RequestHandler = (req, res) => {
     +priceLimit
   );
   if (!result.success) {
-    return res.send(result.message).status(404);
+    return res.status(result.code).send({ message: result.message });
   }
-  return res.send(result.message);
+  return res.send({ message: result.message });
 };
 
 const getEffectivePrice = (
@@ -37,30 +37,32 @@ const getEffectivePrice = (
     return {
       success: false,
       message: 'Book is not loaded yet, please try again later',
+      code: 425,
     };
   }
   operation === 'buy'
     ? book[pair][type].sort((a: Order, b: Order) => a[0] - b[0])
     : book[pair][type].sort((a: Order, b: Order) => b[0] - a[0]);
-  console.log(book[pair][type]);
   for (let i = 0; i < book[pair][type].length; i++) {
     if (priceLimit) {
       if (operation === 'buy') {
+        //if the user gives a limit for a price lower than the lower asks
         if (book[pair][type][0][0] >= priceLimit) {
           return {
             success: false,
             message: 'No order can be completed at that price limit',
+            code: 400,
           };
         }
-        //agregar verificacion si es compra price > priceLimit y si es venta price < priceLimit
 
         if (book[pair][type][i][0] >= priceLimit) {
-          if (i === 0) return;
-          // const aver = (priceLimit - price / acum) / book[pair][type][i][0];
-          console.log('PRICE', price / acum);
-          console.log('ACUM (cantidad comprada)', acum);
-          console.log('ACTUAL', book[pair][type][i][0]);
+          //if the price is higher than the price limit this means the average will go up
 
+          // console.log('PRICE', price / acum);
+          // console.log('ACUM (cantidad comprada)', acum);
+          // console.log('ACTUAL', book[pair][type][i][0]);
+
+          //price condition is checking if the user buys all the available amount at this level, will the priceLimit be exceeded, if not you can continue
           priceCond =
             (price +
               Math.abs(book[pair][type][i][2]) * book[pair][type][i][0]) /
@@ -80,38 +82,28 @@ const getEffectivePrice = (
               success: true,
             };
           }
-
-          //calcular cuanto puedo comprar antes de pasarse
         }
-        //! NOT SURE IF CORRECT
       } else if (type === 'bids') {
-        //verificar para cuando el usuario quiere vender
-        console.log(book[pair][type][0][0]);
-
+        //same as before, if the user wants to sell higher than the highest bid
         if (priceLimit >= book[pair][type][0][0]) {
           return {
             success: false,
             message: 'No order can be completed at that price limit',
+            code: 400,
           };
         }
-
+        //again, if the price is lower than the limit it means the avg price will go down
         if (book[pair][type][i][0] <= priceLimit) {
-          if (i === 0) return;
-          // const aver = (priceLimit - price / acum) / book[pair][type][i][0];
-          // console.log('PRICE', price / acum);
-          // console.log('ACUM', acum);
-          // console.log('ACTUAL', book[pair][type][i][0]);
           priceCond =
             (price + book[pair][type][i][2] * book[pair][type][i][0]) /
             (acum + book[pair][type][i][2]);
-          console.log('PRICE COND', priceCond);
 
+          //if the user sells all the avalaible amount and the result is lower than the priceLimit it means the sell limit is in this level price
           if (priceCond < priceLimit) {
             const max =
               (price - priceLimit * acum) /
               (priceLimit - book[pair][type][i][0]);
 
-            console.log('cantidad que podes vender', max);
             return {
               message: `You can ${operation} ${max} ${pair.slice(
                 0,
@@ -120,18 +112,12 @@ const getEffectivePrice = (
               success: true,
             };
           }
-
-          //calcular cuanto puedo comprar antes de pasarse
         }
       }
     }
     acum += Math.abs(book[pair][type][i][2]);
     price += book[pair][type][i][0] * Math.abs(book[pair][type][i][2]);
-    // console.log('promedio trucho', price / acum);
-
     bought = amount - acum;
-    // console.log(book[pair][type][i][2]);
-    // console.log(acum);
 
     if (acum >= amount) {
       price = price - Math.abs(bought) * book[pair][type][i][0];
@@ -153,5 +139,6 @@ const getEffectivePrice = (
   return {
     success: false,
     message: 'This operation is too big to be completed!',
+    code: 400,
   };
 };
